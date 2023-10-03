9fb2a428ce7ebed6e72b39c87dcf59d1d1bef3df
Naveen <s.naveen7888@gmail.com>
2023-10-03 18:31:13 +0530

    Input status of set and clear updated

--- Core/Src/app/logic/ace_businesslogic.c
+++ Core/Src/app/logic/ace_businesslogic.c
@@ -10,20 +10,20 @@
 typedef struct
 {
 	GPIO_PinState Auto_Manual;
-	uint8_t cycleStart;
-	uint8_t Emergency;
-	uint8_t Ram_close_switch;
-	uint8_t Ram_Open_Switch;
-	uint8_t Tilting_Up_Switch;
-	uint8_t Tilting_down_switch;
-	uint8_t Ejection_In;
-	uint8_t Ejection_Output;
-	uint8_t Slider_In_Switch;
-	uint8_t Slider_Out_Switch;
-	uint8_t Motor_Switch;
-	uint8_t Station_Conformation;
-	uint8_t Robo_Acknowledgment;
-	uint8_t Ram_Close_Sensor;
+	GPIO_PinState cycleStart;
+	GPIO_PinState Emergency;
+	GPIO_PinState Ram_close_switch;
+	GPIO_PinState Ram_Open_Switch;
+	GPIO_PinState Tilting_Up_Switch;
+	GPIO_PinState Tilting_down_switch;
+	GPIO_PinState Ejection_In;
+	GPIO_PinState Ejection_Output;
+	GPIO_PinState Slider_In_Switch;
+	GPIO_PinState Slider_Out_Switch;
+	GPIO_PinState Motor_Switch;
+	GPIO_PinState Station_Conformation;
+	GPIO_PinState Robo_Acknowledgment;
+	GPIO_PinState Ram_Close_Sensor;
 }Hw_Inputs;
 
 typedef struct
@@ -36,31 +36,78 @@ typedef struct
 Hw_Inputs input;
 Hw_Outputs output;
 uint16_t Inputs_Status;
+
 void gdc_businesslogic(void)
 {
 	/*Read the inputs*/
 	input.Auto_Manual 			=	HAL_GPIO_ReadPin(GPIOB,Auto_Manual_Switch_Pin);
-	Inputs_Status = Inputs_Status |(input.Auto_Manual <<0);
+	if(input.Auto_Manual == GPIO_PIN_SET){
+		Inputs_Status = Inputs_Status & ~(1 << 0);
+	}else{
+		Inputs_Status = Inputs_Status |(1 <<0);
+	}
+
 	input.cycleStart 			=	HAL_GPIO_ReadPin(GPIOE,CycleStart_Switch_Pin);
-	Inputs_Status = Inputs_Status |(input.cycleStart <<1);
+	if(input.cycleStart == GPIO_PIN_SET){
+		Inputs_Status = Inputs_Status & ~(1 << 1);
+	}else{
+		Inputs_Status = Inputs_Status |(1 <<1);
+	}
 	input.Emergency 			=	HAL_GPIO_ReadPin(GPIOB,Emergency_Switch_Pin);
-	Inputs_Status = Inputs_Status |(input.Emergency <<2);
+	if(input.Emergency == GPIO_PIN_SET){
+		Inputs_Status = Inputs_Status & ~(1 << 2);
+	}else{
+		Inputs_Status = Inputs_Status |(1 <<2);
+	}
+
 	input.Ram_close_switch 		=	HAL_GPIO_ReadPin(GPIOE,RampClose_Switch_Pin);
-	Inputs_Status = Inputs_Status |(input.Ram_close_switch <<3);
+	if(input.Ram_close_switch == GPIO_PIN_SET){
+		Inputs_Status = Inputs_Status & ~(1 << 3);
+	}else{
+		Inputs_Status = Inputs_Status |(1 <<3);
+	}
 	input.Ram_Open_Switch 		=	HAL_GPIO_ReadPin(GPIOE,RampOpen_Switch_Pin);
-	Inputs_Status = Inputs_Status |(input.Ram_Open_Switch <<4);
+	if(input.Ram_Open_Switch == GPIO_PIN_SET){
+		Inputs_Status = Inputs_Status & ~(1 << 4);
+	}else{
+		Inputs_Status = Inputs_Status |(1 <<4);
+	}
 	input.Tilting_Up_Switch 	=	HAL_GPIO_ReadPin(GPIOE,TiltingUp_Switch_Pin);
-	Inputs_Status = Inputs_Status |(input.Tilting_Up_Switch <<5);
+	if(input.Tilting_Up_Switch == GPIO_PIN_SET){
+		Inputs_Status = Inputs_Status & ~(1 << 5);
+	}else{
+		Inputs_Status = Inputs_Status |(1 <<5);
+	}
 	input.Tilting_down_switch 	=	HAL_GPIO_ReadPin(GPIOE,TiltingDown_Switch_Pin);
-	Inputs_Status = Inputs_Status |(input.Tilting_down_switch <<6);
+	if(input.Tilting_Up_Switch == GPIO_PIN_SET){
+		Inputs_Status = Inputs_Status & ~(1 << 6);
+	}else{
+		Inputs_Status = Inputs_Status |(1 <<6);
+	}
 	input.Ejection_In 			=	HAL_GPIO_ReadPin(GPIOE,EjectionIn_Switch_Pin);
-	Inputs_Status = Inputs_Status |(input.Ejection_In <<7);
+	if(input.Ejection_In == GPIO_PIN_SET){
+		Inputs_Status = Inputs_Status & ~(1 << 7);
+	}else{
+		Inputs_Status = Inputs_Status |(1 <<7);
+	}
 	input.Ejection_Output 		=	HAL_GPIO_ReadPin(GPIOE,EjectionOut_Switch_Pin);
-	Inputs_Status = Inputs_Status |(input.Ejection_Output <<8);
+	if(input.Ejection_Output == GPIO_PIN_SET){
+		Inputs_Status = Inputs_Status & ~(1 << 8);
+	}else{
+		Inputs_Status = Inputs_Status |(1 <<8);
+	}
 	input.Slider_In_Switch 		=	HAL_GPIO_ReadPin(GPIOE,SliderIn_Switch_Pin);
-	Inputs_Status = Inputs_Status |(input.Slider_In_Switch <<9);
+	if(input.Slider_In_Switch == GPIO_PIN_SET){
+		Inputs_Status = Inputs_Status & ~(1 << 9);
+	}else{
+		Inputs_Status = Inputs_Status |(1 <<9);
+	}
 	input.Slider_Out_Switch 	=	HAL_GPIO_ReadPin(GPIOE,SliderOut_Pin);
-	Inputs_Status = Inputs_Status |(input.Slider_Out_Switch <<10);
+	if(input.Slider_Out_Switch == GPIO_PIN_SET){
+		Inputs_Status = Inputs_Status & ~(1 << 10);
+	}else{
+		Inputs_Status = Inputs_Status |(1 <<10);
+	}
 	input.Motor_Switch 			=	0;//HAL_GPIO_ReadPin(GPIOE,Auto_Manual_Switch_Pin);
 	Inputs_Status = Inputs_Status |(input.Motor_Switch <<11);
 	input.Station_Conformation 	=	0;//HAL_GPIO_ReadPin(GPIOE,Auto_Manual_Switch_Pin);
